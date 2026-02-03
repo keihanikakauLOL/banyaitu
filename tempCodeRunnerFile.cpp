59291eb949c5b4cd4a0934af1d27200cfd83c904
keihanikakauLOL <arm.nantana@gmail.com>
2026-02-03 16:23:37 +0700

    insane

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+using namespace std;
+
+void showData(double *,int,int);
+
+void randData(double *,int,int);
+
+void findRowSum(const double *,double *,int,int);
+
+void findColSum(const double *,double *,int,int);
+
+int main(){
+	srand(time(0));
+	const int N = 6, M = 8;
+	double data[N][M] = {};
+	double sum1[N] = {};
+	double sum2[M] = {};
+	double *dPtr = data[0];
+	randData(dPtr,N,M);
+	showData(dPtr,N,M);
+	
+	cout << "---------------------------------------------\n";
+	
+	findRowSum(dPtr,sum1,N,M);
+	showData(sum1,N,1);
+	
+	cout << "---------------------------------------------\n";
+	
+	findColSum(dPtr,sum2,N,M); 
+	showData(sum2,1,M);
+}
+
+void showData(double *dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i);
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr + i) = (rand()%101) / 100.00;
+    }
+}
+
+void findRowSum(const double *dPtr, double *R, int N, int M) {
+	for (int i = 0; i < N; i++) {
+		double sum = 0;
+		for (int j = 0; j < M; j++) {
+			sum += dPtr[(i*M) + j];
+		}
+		*(R + i) = sum;
+	}
+}
+
+void findColSum(const double *dPtr, double *C, int N, int M) {
+	for(int j = 0; j < M; j++) {
+        double sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += dPtr[(i*M) + j];
+        }
+		*(C + j) = sum;
+    }
+}
